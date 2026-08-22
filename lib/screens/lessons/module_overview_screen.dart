cf79c70c9d0a0021f44f7d876fcf5a20c1125e54
Minthe <jassmynecarpio@gmail.com>
2026-08-22 20:48:36 +0800

    fix(imports): add missing google_fonts import in module_overview_screen.dart

--- lib/screens/lessons/module_overview_screen.dart
+++ lib/screens/lessons/module_overview_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:algebrix/core/constants/app_colors.dart';
 import 'package:algebrix/core/constants/app_text_styles.dart';
